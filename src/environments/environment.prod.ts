3c8b54145b0703c16dda8db30000b70a1472a299
Ken Rodenwaldt <kennyro@me.com>
2021-03-12 16:39:59 +0100

    update: add gradient to cover-page, cursor, edit artist-list

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,4 @@
 export const environment = {
   production: true,
-  characters: {
-    spreadsheetId: '11Ai9cZgPjasCPZuarlnam7dLCPvY45LOZ29L29ELmmbU',
-    worksheetId: 1,
-  },
+  apiUrl: 'http://aussicht.space/strapi',
 };
