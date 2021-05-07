24148fefc92fca1d352cb4820fcff58329ce06fb
Ken Rodenwaldt <kennyro@me.com>
2021-05-07 20:36:36 +0200

    machine-essay.comp styling

--- src/app/pages/content_components/machine-essay/machine-essay.component.ts
+++ src/app/pages/content_components/machine-essay/machine-essay.component.ts
@@ -10,6 +10,11 @@ export class MachineEssayComponent implements OnInit {
   @Input() body:any;
   randomMachineTextIndex: any;
   pass_machineGenerated_content:any;
+  pass_machineGenerated_question:any;
+
+  pass_machineGenerated_bundle_01: any;
+  pass_machineGenerated_bundle_02: any;
+  pass_machineGenerated_bundle_03: any;
   machineTextDatabase: any;
   machineTextIndex: any;
 
@@ -18,23 +23,50 @@ export class MachineEssayComponent implements OnInit {
   ngOnInit(): void {    
     this.machineTextDatabase = this.body.json_data;
     this.machineTextIndex = this.machineTextDatabase.length - 1;
-    this.randomMachineTextIndex = Math.floor(Math.random() * this.machineTextIndex);
 
     let i = Math.floor(Math.random() * this.machineTextIndex);
-    this.machineTextDatabase[i].generated
+    let a = Math.floor(Math.random() * this.machineTextIndex);
+    let b = Math.floor(Math.random() * this.machineTextIndex);
+    let c = Math.floor(Math.random() * this.machineTextIndex);
+
+    var wordCounta = this.machineTextDatabase[a].generated;
+    var wordCountb = this.machineTextDatabase[b].generated;
+    var wordCountc = this.machineTextDatabase[c].generated;
     
     this.pass_machineGenerated_content = this.machineTextDatabase[i].generated;
+    this.pass_machineGenerated_question = this.machineTextDatabase[i].input_str;
+
+    this.pass_machineGenerated_bundle_01 = wordCounta.substring(0,500);
+    this.pass_machineGenerated_bundle_02 = wordCountb.substring(0,800);
+    this.pass_machineGenerated_bundle_03 = wordCountc.substring(0,1000);
   }
+  
 
   generateText(){
     this.machineTextDatabase = this.body.json_data;
     this.machineTextIndex = this.machineTextDatabase.length - 1;
-    this.randomMachineTextIndex = Math.floor(Math.random() * this.machineTextIndex);
 
     let i = Math.floor(Math.random() * this.machineTextIndex);
-    this.machineTextDatabase[i].generated
+    let a = Math.floor(Math.random() * this.machineTextIndex);
+    let b = Math.floor(Math.random() * this.machineTextIndex);
+    let c = Math.floor(Math.random() * this.machineTextIndex);
+
+    var wordCounta = this.machineTextDatabase[a].generated;
+    var wordCountb = this.machineTextDatabase[b].generated;
+    var wordCountc = this.machineTextDatabase[c].generated;
     
     this.pass_machineGenerated_content = this.machineTextDatabase[i].generated;
+    this.pass_machineGenerated_question = this.machineTextDatabase[i].input_str;
+
+    this.pass_machineGenerated_bundle_01 = wordCounta.substring(0,500);
+    this.pass_machineGenerated_bundle_02 = wordCountb.substring(0,800);
+    this.pass_machineGenerated_bundle_03 = wordCountc.substring(0,1000);
   }
+
+  generateTextBundle() {
+
+  }
+
+
 
 }
